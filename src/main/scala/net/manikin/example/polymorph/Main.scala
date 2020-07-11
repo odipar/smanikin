799f704813fe6733d11996bc8d6f8fa4d7a8740d
Robbert van Dalen <robbert@availlang.org>
2020-07-11 23:59:21 +0200

    naming

--- src/main/scala/net/manikin/example/polymorph/Main.scala
+++ src/main/scala/net/manikin/example/polymorph/Main.scala
@@ -5,50 +5,50 @@ object Main {
   import net.manikin.core.TransObject._
   import net.manikin.core.context.DefaultContext._
   
-  trait IdBase[O <: DataBase] extends Id[O]
+  trait IdX[O <: XData] extends Id[O]
 
-  case class IdA(i: Long) extends IdBase[A] {
-    def init = new A
+  case class IdA(i: Long) extends IdX[AData] {
+    def init = new AData
   }
 
-  case class IdB(i: Long) extends IdBase[B] {
-    def init = new B
+  case class IdB(i: Long) extends IdX[BData] {
+    def init = new BData
   }
 
-  class DataBase extends Cloneable {
+  class XData extends Cloneable {
     var name: String = ""
-    
+
     // mimicking this.copy(...) on non-case classes
     def copy(f: this.type => Unit): this.type = { val cp = clone().asInstanceOf[this.type] ; f(cp) ; cp }
   }
 
-  class A extends DataBase {
+  class AData extends XData {
     var address: String = ""
 
     override def toString = "A(" + name + "," + address + ")"
   }
 
-  class B extends DataBase {
+  class BData extends XData {
     var age: Long = 0
-    
+
     override def toString = "B(" + name + "," + age + ")"
   }
 
-  trait SetMessage[O <: DataBase] extends Message[IdBase[O], O, Unit] {
+  trait SetMessage[O <: XData] extends Message[IdX[O], O, Unit] {
     def pre = true
     def eff = { }
     def pst = true
   }
-  
-  case class SetName(name: String) extends SetMessage[DataBase] {
+
+  case class SetName(name: String) extends SetMessage[XData] {
     def app = obj.copy(_.name = name)
   }
 
-  case class SetAddress(address: String) extends SetMessage[A] {
+  case class SetAddress(address: String) extends SetMessage[AData] {
     def app = obj.copy(_.address = address)
   }
 
-  case class SetAge(age: Long) extends SetMessage[B] {
+  case class SetAge(age: Long) extends SetMessage[BData] {
     def app = obj.copy(_.age = age)
   }
 
