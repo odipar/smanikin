451af2f368afd1bf4c077c83832e66a2fdc544b1
Robbert van Dalen <robbert@availlang.org>
2020-12-20 10:30:00 +0100

    mergeable Worlds

--- src/main/scala/net/manikin/main/Main.scala
+++ src/main/scala/net/manikin/main/Main.scala
@@ -1,11 +1,12 @@
 package net.manikin.main
 
-import net.manikin.core.TransObject.{Id, Message}
-
 object Main {
 
   trait Id[+O] {
     def init: O
+
+    def v_obj(implicit c: MutableContext): VObj[O] = c.read(this)
+    def old_v_obj(implicit c: MutableContext): VObj[O] = c.read_old(this)
 
     def obj(implicit c: MutableContext): O = c.read(this).obj
     def old_obj(implicit c: MutableContext): O = c.read_old(this).obj
@@ -16,9 +17,10 @@ object Main {
   trait VObj[+O] {
     def obj: O
     def version: Long
+    def fingerPrint: Int
   }
 
-  case class DefaultVObj[+O](obj: O, version: Long) extends VObj[O]
+  case class DefaultVObj[+O](obj: O, version: Long, fingerPrint: Int) extends VObj[O]
 
   trait Msg[+I <: Id[O], O, +R] {
     var _ctx: MutableSelfContext[_ <: Id[O], O] = _
@@ -37,83 +39,97 @@ object Main {
   trait Context
 
   trait MutableContext extends Context {
+    def branch: MutableContext
     def read[O](id: Id[O]): VObj[O]
     def read_old[O](id: Id[O]): VObj[O]
     def send[O, R](id: Id[O], message: Msg[Id[O], O, R]): R
   }
 
-  class DefaultMutableContext(var old: World, var current: World) extends MutableContext {
+  class DefaultMutableContext(val init: World) extends MutableContext with Cloneable {
+    var old: World = init
+    var current: World = init
+
+    private def copySelf: DefaultMutableContext = this.clone().asInstanceOf[DefaultMutableContext]
+
+    def branch: MutableContext = { val s = copySelf ; s.current = s.current.branch ; s }
     def read[O](id: Id[O]): VObj[O] = { val (obj, w2) = current.read(id) ; current = w2 ; obj }
     def read_old[O](id: Id[O]): VObj[O] = { val (obj, w2) = old.read(id) ; old = w2 ; obj }
     def send[O, R](id: Id[O], message: Msg[Id[O], O, R]): R = {
-      val (result, w2) = current.send(id, message)
-      current = w2
-      result
+      val (result, w2) = current.send(id, message) ; current = w2 ; result
     }
+
+    def withCurrent(w: World): Unit = { old = init ; current = w }
   }
 
   trait MutableSelfContext[+I <: Id[O], O] extends MutableContext {
     def self: I
   }
 
-  class DefaultMutableSelfContext[+I <: Id[O], O](val self: I, old: World, current: World)
-    extends DefaultMutableContext(old, current) with MutableSelfContext[I, O]
+  class DefaultMutableSelfContext[+I <: Id[O], O](val self: I, init: World)
+    extends DefaultMutableContext(init) with MutableSelfContext[I, O]
 
   type STATE = Map[Id[_], VObj[_]]
 
   trait World {
     def read[O](id: Id[O]): (VObj[O], World)
     def send[O, R](id: Id[O], message: Msg[Id[O], O, R]): (R, World)
+
+    def branch: World
     def merge(other: World): World
 
     def actions: Vector[ACTION[_]]
     def latest(state: STATE): STATE
   }
 
-  case class DefaultWorld(state: STATE = Map(), actions: Vector[ACTION[_]] = Vector()) extends World {
-    def get[O](id: Id[O]): VObj[O] = state.getOrElse(id, DefaultVObj(id.init, 0)).asInstanceOf[VObj[O]]
-    def read[O](id: Id[O]): (VObj[O], World) = {
-      val v_obj = get(id)
-      val read = DefaultRead[Id[O], O](id, v_obj.version)
-      (v_obj, this.copy(actions = actions :+ read))
+  def vobj[O](id: Id[O]): VObj[O] = {
+    val obj = id.init
+    DefaultVObj(obj, 0, obj.hashCode())
+  }
+
+  import scala.util.hashing.MurmurHash3._
+
+  case class DefaultWorld(parent: World = null, state: STATE = Map(), actions: Vector[ACTION[_]] = Vector()) extends World {
+    def branch: World = DefaultWorld(this, Map(), Vector())
+    def get[O](id: Id[O]): VObj[O] = state.getOrElse(id, vobj(id)).asInstanceOf[VObj[O]]
+    def read[O](id: Id[O]): (VObj[O], World) = read2(id)
+    def read2[O](id: Id[O]): (VObj[O], DefaultWorld) = {
+      val v_obj = {
+        if (!state.contains(id) && parent != null) parent.latest(Map(id -> get(id)))(id).asInstanceOf[VObj[O]]
+        else get(id)
+      }
+      val read = DefaultRead[Id[O], O](id, v_obj.version, v_obj.fingerPrint)
+      (v_obj, this.copy(state = state + (id -> v_obj), actions = actions :+ read))
     }
     def send[O, R](id: Id[O], message: Msg[Id[O], O, R]): (R, World) = {
-      val old_v_obj = get(id)
-      val read_only_world = ReadOnlyWrapper(DefaultWorld(state, Vector()))
+      val (old_v_obj, latest) = read2(id)
+      val read_only_world = ReadOnlyWrapper(latest.branch)
 
-      val c = new DefaultMutableSelfContext[Id[O], O](id, read_only_world, read_only_world)
+      val c = new DefaultMutableSelfContext[Id[O], O](id, read_only_world)
       message._ctx = c
 
       if (!message.pre) throw new RuntimeException("Pre failed")
       else {
         val preReadActions = (c.old.actions ++ c.current.actions).map(_.asInstanceOf[READ[_]]) // only reads allowed
 
-        c.old = read_only_world
-        c.current = read_only_world
-        
+        c.withCurrent(read_only_world)
         val new_obj = message.app
-
         val appReadActions = (c.old.actions ++ c.current.actions).map(_.asInstanceOf[READ[_]]) // only reads allowed
         if (!appReadActions.forall(_.id == id)) throw new RuntimeException("app may only read itself")
 
-        val new_state = state + (id -> DefaultVObj(new_obj, old_v_obj.version + 1))
-
-        c.old = read_only_world
-        c.current = DefaultWorld(new_state, Vector())
-
+        val new_state = state + (id -> DefaultVObj(new_obj, old_v_obj.version + 1, mix(old_v_obj.fingerPrint, message.hashCode())))
+        c.withCurrent(DefaultWorld(this, new_state, Vector()))
         val result = message.eff
         val effActions = c.old.actions.map(_.asInstanceOf[READ[_]]) ++ c.current.actions // only read actions in old
 
-        c.old = read_only_world
-        c.current = ReadOnlyWrapper(c.current)
+        c.withCurrent(ReadOnlyWrapper(c.current))
 
         if (!message.pst) throw new RuntimeException("Post failed")
         else {
           val pstActions = (c.old.actions ++ c.current.actions).map(_.asInstanceOf[READ[_]]) // only reads allowed
-          val send = DefaultSend(id, old_v_obj.version, message, preReadActions, effActions, result, pstActions)
+          val send = DefaultSend(id, old_v_obj.version, old_v_obj.fingerPrint, message, preReadActions, effActions, result, pstActions)
           val sub_state = c.current.latest(send.minWrites.map(_.id).map(id => (id, get(id))).toMap)
 
-          (result, this.copy(state = new_state ++ sub_state, actions = actions :+ send))
+          (result, latest.copy(state = new_state ++ sub_state, actions = actions :+ send))
         }
       }
     }
@@ -134,38 +150,50 @@ object Main {
       val minReads = minVIds(postfix_actions.flatMap(_.minReads))
       val minWrites = minVIds(postfix_actions.flatMap(_.minWrites))
 
-      if ((minReads ++ minWrites).exists(p => get(p.id).version != p.version)) throw new RuntimeException("CANNOT MERGE")
+      if ((minReads ++ minWrites).exists{p =>
+        val v_obj = get(p.id)
+        v_obj.version != p.version || v_obj.fingerPrint != p.fingerPrint
+      }) throw new RuntimeException("CANNOT MERGE")
 
       val sub_state = other.latest(minWrites.map(_.id).map(id => (id, get(id))).toMap)
 
-      DefaultWorld(state ++ sub_state, actions ++ postfix_actions)
+      DefaultWorld(this, state ++ sub_state, actions ++ postfix_actions)
     }
 
-    def latest(state: STATE): STATE = state.keys.map(id => (id, get(id))).toMap
+    def latest(other: STATE): STATE = {
+      other.keys.map { id =>
+        val v_obj = {
+          if (!state.contains(id) && parent != null) parent.latest(Map(id -> get(id)))(id)
+          else get(id)
+        }
+        (id, v_obj)
+      }.toMap
+    }
   }
 
   case class ReadOnlyWrapper(other: World, actions: Vector[READ[_]]= Vector()) extends World {
+    def branch: World = throw new RuntimeException("Cannot branch Read Only World")
     def read[O](id: Id[O]): (VObj[O], World) = {
       val (v_obj, n_other) = other.read(id)
-      (v_obj, ReadOnlyWrapper(n_other, actions :+ DefaultRead[Id[O], O](id, v_obj.version)))
+      (v_obj, ReadOnlyWrapper(n_other, actions :+ DefaultRead[Id[O], O](id, v_obj.version, v_obj.fingerPrint)))
     }
     def send[O, R](id: Id[O], message: Msg[Id[O], O, R]): (R, World) = throw new RuntimeException("Cannot send to Read Only World")
     def merge(other: World): World = throw new RuntimeException("Cannot merge to Read Only World")
     def latest(state: STATE): STATE = other.latest(state)
   }
 
-  case class VId[+O](id: Id[O], version: Long)
+  case class VId[+O](id: Id[O], version: Long, fingerPrint: Int)
 
   trait Action[+I <: Id[O], O] {
     def id: I
     def version: Long
+    def fingerPrint: Int
 
     def minReads: Set[VId[_]]
     def minWrites: Set[VId[_]]
   }
 
   trait Read[+I <: Id[O], O] extends Action[I, O] {
-    def minReads = Set(VId(id, version))
     def minWrites = Set()
   }
 
@@ -176,22 +204,25 @@ object Main {
     def preActions: Seq[READ[_]]
     def effActions: Seq[ACTION[_]]
     def pstActions: Seq[READ[_]]
-
-    def minReads = minVIds(VId(id, version) +: (preActions ++ effActions ++ pstActions).flatMap(_.minReads))
-    def minWrites = minVIds(VId(id, version) +: effActions.flatMap(_.minWrites))
   }
 
-  case class DefaultRead[+I <: Id[O], O](id: I, version: Long) extends Read[I, O]
+  case class DefaultRead[+I <: Id[O], O](id: I, version: Long, fingerPrint: Int) extends Read[I, O] {
+    def minReads = Set(VId(id, version, fingerPrint))
+  }
 
   case class DefaultSend[+I <: Id[O], O, +R](
     id: I,
     version: Long,
+    fingerPrint: Int,
     message: Msg[I, O, R],
     preActions: Seq[READ[_]],
     effActions: Seq[ACTION[_]],
     effResult: R,
     pstActions: Seq[READ[_]]
-  ) extends Send[I, O, R]
+  ) extends Send[I, O, R] {
+    def minReads = minVIds(VId(id, version, fingerPrint) +: (preActions ++ effActions ++ pstActions).flatMap(_.minReads))
+    def minWrites = minVIds(VId(id, version, fingerPrint) +: effActions.flatMap(_.minWrites))
+  }
 
   type READ[O] = Read[_ <: Id[O], O]
   type SEND[O] = Send[_ <: Id[O], O, _]
@@ -250,7 +281,7 @@ object Main {
   }
 
   def main(args: Array[String]): Unit = {
-    implicit var w = new DefaultMutableContext(DefaultWorld(), DefaultWorld())
+    implicit var w = new DefaultMutableContext(DefaultWorld())
 
     val a1 = AccountId("A1")
     val a2 = AccountId("A2")
@@ -262,21 +293,22 @@ object Main {
 
     a1 ! OpenAccount(100)
     a2 ! OpenAccount(50)
-    t1 ! Book(a1, a2, 30)
 
-    val w1 = w.current
+    val w0 = w.current
 
-    a3 ! OpenAccount(80)
-    a4 ! OpenAccount(40)
-    t2 ! Book(a3, a4, 30)
+    w = new DefaultMutableContext(DefaultWorld())
 
+    a3 ! OpenAccount(40)
+    a4 ! OpenAccount(60)
+    a4 ! Withdraw(30)
     val w2 = w.current
+    
+    println("w0: " + w0.actions)
+    println("w2: " + w2.actions)
+    
+    w.current = w0.merge(w2)
 
-    val w3 = w1.merge(w2)
-
-    println("a1: " + w3.read(a1)._1)
-    println("a2: " + w3.read(a2)._1)
-    println("a3: " + w3.read(a3)._1)
-    println("a4: " + w3.read(a4)._1)
+    println("w3: " + a1.obj)
+    w.current
   }
 }
